018bc801d9b1d37403e7685656d1e8044ce639dd
jmorales <jmorales@jmorales-VirtualBox>
2013-10-01 17:25:26 +0200

    Funciona encriptacion y desencriptacion de palabras y frases!!!

--- cesar.c
+++ cesar.c
@@ -12,22 +12,48 @@
 void main(){
 	char string[MAX],string2[MAX];
 	char c;
-	int dim=0,i=0,movimiento=0;
-	printf("Movimiento:");
-	scanf("%d",&movimiento);
-	getchar();
-	printf("Dime la frase a mover:");
-	gets(string);
-	puts(string);
-	dim=strlen(string);
-	while (i!=dim){
-		c = string[i]+movimiento;
-		string2[i] = c;
-		i++;
+	int dim=0,i=0,movimiento=0,op=0;
+	printf("Encriptacion del Cesar\n");
+	printf("Que deseas hacer?\n");
+	printf("1.Encriptar\n");
+	printf("2.Desencriptar\n");
+	scanf("%d",&op);
+	if (op==1){
+		printf("Movimiento:");
+		scanf("%d",&movimiento);
+		getchar();
+		printf("Dime la frase a encriptar:");
+		gets(string);
+		puts(string);
+		dim=strlen(string);
+		while (i!=dim){
+			c = string[i]+movimiento;
+			string2[i] = c;
+			i++;
+		}
+		if (i>=dim){
+			string2[i]='\0';
+		}
+		puts(string2);
 	}
-	if (i>=dim){
-		string2[i]='\0';
+	if (op==2){
+		printf("Movimiento:");
+		scanf("%d",&movimiento);
+		getchar();
+		printf("Dime la frase a desencriptar:");
+		gets(string);
+		puts(string);
+		dim=strlen(string);
+		while (i!=dim){
+			c = string[i]-movimiento;
+			string2[i] = c;
+			i++;
+		}
+		if (i>=dim){
+			string2[i]='\0';
+		}
+		puts(string2);
 	}
-	puts(string2);
+
 }
 
